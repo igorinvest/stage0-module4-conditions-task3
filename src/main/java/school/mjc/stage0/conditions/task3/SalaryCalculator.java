5b960ccbe50d496eaca94f4559db23814b5da4b1
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-26 15:24:57 +0530

    Don't look inside

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        //Basic Salary <= 10000: taxes - 15%.
+        // 10000 < Basic Salary <= 20000: taxes - 18%
+        // Basic Salary > 20000 : taxes - 20%
+        // Basic salary < 0 -> "wrong input!"
+        double rate;
+        if(salary > 0 & salary <= 10000) {
+            rate = 0.15;
+        } else if (salary > 10000 & salary <= 20000) {
+            rate = 0.18;
+        } else if (salary > 20000) {
+            rate = 0.2;
+        } else {
+            System.out.println("wrong input!");
+            return;
+        }
+        double afterTax = salary - (salary * rate);
+        System.out.println(afterTax);
     }
 }
